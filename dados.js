50549e305d4630fc1a5c17ada41fb95100b69e5e
Jonnas Cipriano <jonnascipriano@gmail.com>
2024-09-04 20:57:47 -0300

    Aula 3 concluída

---
+++ dados.js
@@ -0,0 +1,16 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, conhecida como a Fada do Skate, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e medalhas, tornando-se um dos maiores nomes do skate mundial. Sua habilidade e estilo único a tornaram um ícone para a nova geração de skatistas. Rayssa é um exemplo de que não há limites para os sonhos.",
+        link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal'
+    },{
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com uma carreira promissora, ela já conquistou diversas medalhas em competições nacionais e internacionais. Sua dedicação aos treinos e sua paixão pelo judô a tornam uma atleta inspiradora.",
+        link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza' 
+    },{
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com suas acrobacias e beleza. Ela conquistou diversas medalhas em competições importantes, como os Jogos Olímpicos. Sua força e determinação a tornaram um símbolo do esporte feminino no Brasil.",
+        link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade' 
+
+    }
+]
